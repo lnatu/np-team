af0ca63fafd6dafb469489b81f5e7022fd659fee
Tu <tu.lna@gnt.com.vn>
2020-03-16 16:58:36 +0700

    7th commit

--- src/js/index.js
+++ src/js/index.js
@@ -132,3 +132,89 @@ Navbar.prototype.toggleNavbar = function() {
 
 var navbar = new Navbar();
 navbar.toggleNavbar();
+
+var Progress = function() {
+  this.progressBars = document.querySelectorAll('.progress-bar');
+}
+Progress.prototype.init = function() {
+  var progressBars = this.progressBars;
+  for (var i = 0; i < progressBars.length; i++) {
+    var _this = progressBars[i];
+    var valueNow = parseFloat(_this.dataset.valuenow);
+    var valueMin = parseFloat(_this.dataset.valuemin);
+    var valueMax = parseFloat(_this.dataset.valuemax);
+
+    if (valueNow < valueMin) {
+      valueNow = 0;
+    }
+
+    if (valueNow > valueMax) {
+      valueNow = 100;
+    } else {
+      valueNow = Math.floor((valueNow / valueMax) * 100);
+    }
+
+    valueNow = valueNow + '%';
+
+    var showText = parseInt(_this.dataset.showtext);
+    _this.style.width = valueNow;
+    if (showText && showText === 1) {
+      _this.innerText = valueNow;
+    }
+  }
+};
+
+var progress = new Progress();
+progress.init();
+
+
+var Notification = function() {
+  this.toggleBtn = document.querySelectorAll('button[data-notification]');
+  this.hideNoti = document.querySelectorAll('button[data-offnotification]');
+  this.notifications = document.querySelectorAll('.notification');
+};
+
+Notification.prototype.initNotification = function() {
+  var notis = this.notifications;
+  for (var i = 0; i < notis.length; i++) {
+    var _this = notis[i];
+    [x, y] = _this.dataset.position.split('-');
+    _this.setAttribute('style', x + ':30px;' + y +':30px');
+  }
+};
+
+Notification.prototype.showNotification = function() {
+  var btns = this.toggleBtn;
+  for (var i = 0; i < btns.length; i++) {
+    var _this = btns[i];
+    _this.addEventListener('click', function(e) {
+      e.preventDefault();
+      var _thisClicked = this;
+      document.querySelector(_thisClicked.dataset.notification).classList.add('reveal');
+      setTimeout(function() {
+        document.querySelector(_thisClicked.dataset.notification).classList.remove('reveal');
+      }, 5000);
+    });
+  }
+};
+
+Notification.prototype.offNotification = function() {
+  var btns = this.hideNoti;
+  for (var i = 0; i < btns.length; i++) {
+    var _this = btns[i];
+    _this.addEventListener('click', function(e) {
+      e.preventDefault();
+      var _thisClicked = this;
+      document.querySelector(_thisClicked.dataset.offnotification).classList.remove('reveal');
+    });
+  }
+};
+
+Notification.prototype.initEvents = function() {
+  this.initNotification();
+  this.showNotification();
+  this.offNotification();
+};
+
+var noti = new Notification();
+noti.initEvents();
